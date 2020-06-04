192b2f5e5b2be3e8236e865f278112bbc99e5ac7
saiteja-gatadi1996 <42731246+saiteja-gatadi1996@users.noreply.github.com>
2020-06-04 22:16:42 +0530

    Add files via upload

---
+++ limits-service/limits-service/src/main/java/com/golive/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,32 @@
+package com.golive.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@RestController
+public class LimitsConfigurationController {
+
+@Autowired
+private Configuration configuration;
+
+@GetMapping("/limits")
+public LimitConfiguration retrieveLimits() {
+	return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+
+	
+}
+@GetMapping("/fault-tolerance-example")
+@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+
+public LimitConfiguration retrieveConfiguration(){
+throw new RuntimeException("not available");
+}
+
+
+public LimitConfiguration fallbackRetrieveConfiguration(){
+return new LimitConfiguration(999,9);
+}
+}
---
+++ spring-cloud-config-server/spring-cloud-config-server/HELP.md
@@ -0,0 +1,19 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The original package name 'com.golive.microservices.spring-cloud-config-server' is invalid and this project uses 'com.golive.microservices.springcloudconfigserver' instead.
+
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.1.14.RELEASE/maven-plugin/)
+* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/2.3.0.RELEASE/reference/htmlsingle/#using-boot-devtools)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Centralized Configuration](https://spring.io/guides/gs/centralized-configuration/)
+
